54693374b854c5d2321b71d22f320a8cc0426f5b
Neradoc <neraOnGit@ri1.fr>
2023-04-12 00:25:20 +0200

    keypad version, fix example

--- analog_button.py
+++ analog_button.py
@@ -1,8 +1,113 @@
+from supervisor import ticks_ms
 import time
 
-NO_KEY = 0
-UNKNOWN = -1
-UNSTABLE = -2
+NO_KEY = -1
+UNKNOWN = -2
+UNSTABLE = -3
+
+class Event:
+    """
+    A key transition event.
+
+    :param int key_number: the key number
+    :param bool pressed: ``True`` if the key was pressed; ``False`` if it was released.
+    :param int timestamp: The time in milliseconds that the keypress occurred in the
+                          `supervisor.ticks_ms` time system.  If specified as None,
+                          the current value of `supervisor.ticks_ms` is used.
+    """
+
+    def __init__(self, key: int, pressed: bool, timestamp: int = None):
+        self.key_number = key
+        """The key number."""
+        self.timestamp = timestamp or ticks_ms()
+        """The timestamp."""
+        self.pressed = pressed
+        """True if the event represents a key down (pressed) transition.
+        The opposite of released."""
+
+    @property
+    def released(self) -> bool:
+        """True if the event represents a key up (released) transition.
+        The opposite of pressed."""
+        return not self.pressed
+
+    def __eq__(self, other: object) -> bool:
+        """Two Event objects are equal if their key_number and pressed/released values
+        are equal. Note that this does not compare the event timestamps."""
+        return self.key_number == other.key_number and self.pressed == other.pressed
+
+    def __hash__(self) -> int:
+        """Returns a hash for the Event, so it can be used in dictionaries, etc..
+        Note that as events with different timestamps compare equal,
+        they also hash to the same value."""
+        return self.key_number << 1 + int(self.pressed)
+
+    def __repr__(self) -> str:
+        """Human readble repr."""
+        pressed = "pressed" if self.pressed else "released"
+        return f"<Event {self.key_number} {pressed} {self.timestamp}>"
+
+class EventQueue:
+    """
+    A queue of Event objects, filled by a scanner.
+    """
+
+    def __init__(self):  # , max_events=64):
+        self._outq = []
+        self._inq = []
+
+    def append(self, event: Event) -> None:
+        """Append an event at the end of the queue"""
+        self._inq.append(event)
+
+    def get(self) -> Event:
+        """
+        Return the next key transition event.
+        Return None if no events are pending.
+        """
+        if self._outq:
+            return self._outq.pop()
+        if len(self._inq) == 1:
+            return self._inq.pop()
+        if self._inq:
+            self._outq = list(reversed(self._inq))
+            self._inq.clear()
+            return self._outq.pop()
+        return None
+
+    def get_into(self, event: Event) -> bool:
+        """
+        Store the next key transition event in the supplied event, if available,
+        and return True. If there are no queued events, do not touch event
+        and return False.
+        Note: in python this does not optimize to avoid allocating.
+        """
+        next_event = self.get()
+        if next_event:
+            event.key_number = next_event.key_number
+            event.timestamp = next_event.timestamp
+            event.pressed = next_event.pressed
+            return True
+        return False
+
+    def clear(self) -> None:
+        """Clear any queued key transition events."""
+        self._outq.clear()
+        self._inq.clear()
+
+    def __bool__(self) -> bool:
+        """
+        True if len() is greater than zero.
+        This is an easy way to check if the queue is empty.
+        """
+        return len(self) > 0
+
+    def __len__(self) -> int:
+        """
+        Return the number of events currently in the queue.
+        Used to implement len().
+        """
+        return len(self._outq) + len(self._inq)
 
 class AnalogKeypad:
     def __init__(self, pin, neutral, *buttons, tolerance=0x400, stability_delay=0.002):
@@ -13,7 +118,8 @@ class AnalogKeypad:
         self._steps = (neutral,) + buttons
         self.tolerance = tolerance
         self.delay = stability_delay
-        self.status = 0
+        self.events = EventQueue()
+        self._status = NO_KEY
 
     @property
     def button(self):
@@ -23,9 +129,29 @@ class AnalogKeypad:
         temp = self.analog()
         if abs(value - temp) > self.tolerance:
             return UNSTABLE
-        # print(f"{value:5d} {value >> 8:3d} <{value:04X}> [{value:016b}] B: {button}")
         for btn, step in enumerate(self._steps):
             if abs(value - step) < self.tolerance:
-                return btn
+                return btn - 1
         return UNKNOWN
+
+    @property
+    def buttons(self):
+        button = self.button
+        if button >= 0:
+            return [button]
+        return []
+
+    def update(self, value=None):
+        timestamp = ticks_ms()
+        status = self._status
+        if value is not None:
+            current_button = value
+        else:
+            current_button = self.button
+        if status != current_button:
+            if status >= 0:
+                self.events.append(Event(status, False, timestamp))
+            if current_button >= 0:
+                self.events.append(Event(current_button, True, timestamp))
+        self._status = current_button
 
--- example/analog_button_simpletest.py
+++ example/analog_button_simpletest.py
@@ -5,18 +5,18 @@ This is using a Feather RP2040.
 import analogio
 import board
 import time
-from analog_button import AnalogKeypad
+from analog_button import AnalogKeypad, NO_KEY
 
 analog_input = analogio.AnalogIn(board.A0)
 buttons_values = (0, 26310, 39300, 52100, 65535)
 keys = AnalogKeypad(analog_input, *buttons_values)
 
-current_button = 0
+current_button = NO_KEY
 while True:
-    button = keys_A.button
+    button = keys.button
     if current_button != button:
         if button >= 0:
             print(f"Button {button} pressed")
         elif button < 0 and current_button >= 0:
-            print(f"Button {button} released")
+            print(f"Button {current_button} released")
     current_button = button
